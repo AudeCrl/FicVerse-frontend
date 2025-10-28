f382ac54fe25ff688975e5787ec71686b2239b50
Elian <elian.alphonse@gmail.com>
2025-10-28 23:40:31 +0100

    App.js remis à l'origine

--- App.js
+++ App.js
@@ -4,10 +4,20 @@ import store from './store';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-import ProfileScreen from './screens/ProfileScreen';
-
 export default function App() {
   return (
-    <ProfileScreen />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
