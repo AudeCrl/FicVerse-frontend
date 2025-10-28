1f7dd9e8480f1033c32ebdf7e3606dcb2cf584ab
Aude <aude.curial@gmail.com>
2025-10-28 16:27:27 +0100

    Creation files structure screens, reducers & store.js

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import { Provider } from 'react-redux';
+import store from './store';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
---
+++ store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import user from './reducers/user';
+
+export default configureStore({
+    reducer: { user },
+});
