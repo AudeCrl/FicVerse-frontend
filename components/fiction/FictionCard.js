9f9534fa514946e64f169486bf12b0d286a15518
Aude <aude.curial@gmail.com>
2025-11-03 09:15:41 +0100

    [FIX] Modify import of Tags.js

--- components/fiction/FictionCard.js
+++ components/fiction/FictionCard.js
@@ -4,7 +4,7 @@ import { useTheme } from '../../context/ThemeContext.js';
 import { typography } from '../../styles/globalStyles.js';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import Tags from './Tags.js';
+import Tags from './Tags';
 import * as Linking from "expo-linking";
 
 export default function FictionCard({ fiction, collapsingState, showReadingStatus }) {
@@ -134,7 +134,7 @@ export default function FictionCard({ fiction, collapsingState, showReadingStatu
                 </View>
             </View>
         ) : null;
-
+        console.log('TAGS', fiction.title, '=>', fiction.tags);
     return (
         <View style={styles.fictionCard}>
             {showReadingStatus &&
